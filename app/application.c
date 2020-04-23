b5a132c09649db2431122dbfe64c8c381e219b51
Pavel Hübner <pavel.hubner@bigclown.com>
2020-04-23 14:03:29 +0200

    Fix SDK link

--- app/application.c
+++ app/application.c
@@ -1,6 +1,6 @@
 #include <application.h>
 
-// Find defailed API description at https://sdk.bigclown.com/
+// Find defailed API description at https://sdk.hardwario.com/
 
 // LED instance
 bc_led_t led;
