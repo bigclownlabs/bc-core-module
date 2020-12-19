131e60084ad9d465d653fbd7d0ca1c5dbc843420
Jan Janak <jan@janakj.org>
2020-12-18 23:04:52 -0500

    Pull in the latest SDK

--- app/application.c
+++ app/application.c
@@ -3,22 +3,22 @@
 // Find defailed API description at https://sdk.hardwario.com/
 
 // LED instance
-bc_led_t led;
+twr_led_t led;
 
 // Button instance
-bc_button_t button;
+twr_button_t button;
 
 // Button event callback
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
+void button_event_handler(twr_button_t *self, twr_button_event_t event, void *event_param)
 {
     // Log button event
-    bc_log_info("APP: Button event: %i", event);
+    twr_log_info("APP: Button event: %i", event);
 
     // Check event source
-    if (event == BC_BUTTON_EVENT_PRESS)
+    if (event == TWR_BUTTON_EVENT_PRESS)
     {
         // Toggle LED pin state
-        bc_led_set_mode(&led, BC_LED_MODE_TOGGLE);
+        twr_led_set_mode(&led, TWR_LED_MODE_TOGGLE);
     }
 }
 
@@ -26,15 +26,15 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
 void application_init(void)
 {
     // Initialize logging
-    bc_log_init(BC_LOG_LEVEL_DUMP, BC_LOG_TIMESTAMP_ABS);
+    twr_log_init(TWR_LOG_LEVEL_DUMP, TWR_LOG_TIMESTAMP_ABS);
 
     // Initialize LED
-    bc_led_init(&led, BC_GPIO_LED, false, 0);
-    bc_led_set_mode(&led, BC_LED_MODE_ON);
+    twr_led_init(&led, TWR_GPIO_LED, false, 0);
+    twr_led_set_mode(&led, TWR_LED_MODE_ON);
 
     // Initialize button
-    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, 0);
-    bc_button_set_event_handler(&button, button_event_handler, NULL);
+    twr_button_init(&button, TWR_GPIO_BUTTON, TWR_GPIO_PULL_DOWN, 0);
+    twr_button_set_event_handler(&button, button_event_handler, NULL);
 }
 
 // Application task function (optional) which is called peridically if scheduled
@@ -43,8 +43,8 @@ void application_task(void)
     static int counter;
 
     // Log task run and increment counter
-    bc_log_debug("APP: Task run (count: %d)", ++counter);
+    twr_log_debug("APP: Task run (count: %d)", ++counter);
 
     // Plan next run of this task in 1000 ms
-    bc_scheduler_plan_current_from_now(1000);
+    twr_scheduler_plan_current_from_now(1000);
 }
